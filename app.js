a4f743da7dc73ab9ee1f860932df5232ea2113e9
kylehovey <khoveyc@gmail.com>
2020-09-13 14:30:35 -0600

    Transformed some of the data for better readout

--- app.js
+++ app.js
@@ -20,12 +20,53 @@ const serialPort = new SerialPort(serialPath, { baudRate: 9600 });
 const parser = new Readline();
 serialPort.pipe(parser);
 
+const when = ([key]) => fn => (inst, point) => inst === key ? fn(point) : point;
+const displayAs = ([unit]) => () => ({ unit });
+
+const rules = [
+  when`pm10`(displayAs`µg/m³`),
+  when`pm25`(displayAs`µg/m³`),
+  when`pm100`(displayAs`µg/m³`),
+  when`CO2`(displayAs`Parts Per Million`),
+  when`TVOC`(displayAs`Parts Per Billion`),
+  when`pressure`(({ unit, value }) => ({
+    unit: 'Atmospheres',
+    value: (value * 0.00000987).toFixed(2),
+  })),
+  when`temperature`(({ unit, value }) => ({
+    unit: '°F',
+    value: (value * 9/5 + 32).toFixed(2),
+  })),
+];
+
+const chain = rules
+  .reduce((acc, fn) => (key, point) => fn(key, acc(key, point)), (a, b) => b);
+
+const transform = (data) => Object
+  .entries(data)
+  .reduce((acc, [key, point]) => ({
+    ...acc,
+    [key]: {
+      ...point,
+      ...chain(key, point),
+    },
+  }), {});
+
+const extras = [
+];
+
 parser.on('data', data => {
   const parsed = JSON.parse(data);
 
   SensorPoint.create(parsed)
 
-  io.emit('data-point', parsed);
+  io.emit('data-point', {
+    ...transform(parsed),
+    ...extras.reduce((acc, fn) => ({
+      ...acc,
+      ...fn(parsed),
+    }), {})
+  });
 });
 
 app.use(bodyParser.json());
